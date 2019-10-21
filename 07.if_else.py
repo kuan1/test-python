aac7904ddcb60978c700cf4c1dc940d77f30ef50
luzhongkuan <luzhongk@126.com>
2019-10-21 22:03:00 +0800

    code

---
+++ 07.if_else.py
@@ -0,0 +1,7 @@
+# if语句
+a = 10
+b = 11
+if a > b:
+    print(f'{a}大于{b}')
+else:
+    print(f'{b}大于{a}')
---
+++ code/06.条件运算.py
@@ -0,0 +1,6 @@
+# 三元运算符
+a = 1
+b = 5
+c = 3
+max = a if (a > b and a > c) else (b if b > c else c)
+print(max)
