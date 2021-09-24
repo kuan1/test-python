46e84689d19a483acb0d18c30571beb2228e5c2a
luzhongkuan <luzhongk@126.com>
2021-09-24 20:31:52 +0800

    feat(30days): day09_conditionals

---
+++ 30days/day09_conditionals/README.md
@@ -0,0 +1,3 @@
+# conditionals
+
+https://github.com/Asabeneh/30-Days-Of-Python/blob/master/09_Day_Conditionals/09_conditionals.md
---
+++ 30days/day09_conditionals/index.py
@@ -0,0 +1,25 @@
+# If Condition
+if 1 > -1:
+  print(True)
+
+# If Else
+if 1 > 2:
+  print('error')
+else:
+  print('1 < 2')
+
+# If Elif Else
+if 1 > 2:
+  print('error')
+elif 2 == 2:
+  print('2 == 2')
+else:
+  print('error')
+
+# If Condition and Logical Operators
+if 1 < 2 and 3 < 4:
+  print('1 < 2 and 3 < 4')
+
+# If and Or Logical Operators
+if 0 > 3 or 1 > 0:
+  print('0 > 3 or 1 > 0')
