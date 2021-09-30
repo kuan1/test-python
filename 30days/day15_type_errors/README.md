660f72f01d65aedcb84701354dec40923ef2dc23
luzhongkuan <luzhongk@126.com>
2021-09-30 11:58:05 +0800

    feat(30days): type error

---
+++ 30days/day15_type_errors/README.md
@@ -0,0 +1,3 @@
+# type errors
+
+https://github.com/Asabeneh/30-Days-Of-Python/blob/master/15_Day_Python_type_errors/15_python_type_errors.md
---
+++ 30days/day15_type_errors/index.py
@@ -0,0 +1,32 @@
+# SyntaxError
+# print 'Hello Python'
+
+# NameError
+# print(age)
+
+# IndexError
+# print([1,2,3,4][10])
+
+# ModuleNotFoundError
+# import maths
+
+# AttributeError
+# import math
+# print(math.PI)
+
+# KeyError
+# user = {'name': 'kuan'}
+# print(user['name'])
+# print(user['age'])
+
+# TypeError
+# print(4 + '3')
+
+# ImportError
+# from math import power
+
+# ValueError
+# int('12a')
+
+# ZeroDivisionError
+# 1 / 0
